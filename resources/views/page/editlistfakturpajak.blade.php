12f0fa413f2531d8b3967b4f2e44084378bf0083
Arrizal F.B <arrizalfauzanb@gmail.com>
2020-07-14 12:57:14 +0700

    Update editlistfakturpajak.blade.php

--- resources/views/page/editlistfakturpajak.blade.php
+++ resources/views/page/editlistfakturpajak.blade.php
@@ -49,7 +49,20 @@
                 </div>
                 <div class="form-group">
                   <label for="bulanpajak">Bulan Pajak </label>
-                  <input type="text" class="form-control" value="{{$listfaktur->bulanpajak}}"id="bulanpajak" name="bulanpajak" placeholder="Bulan Pajak">
+                  <select class="form-control" name="bulanpajak" id="bulanpajak">
+                    <option value="Januari-01">Januari-01</option>
+                    <option value="Februari-02">Februari-02</option>
+                    <option value="Maret-03">Maret-03</option>
+                    <option value="April-04">April-04</option>
+                    <option value="Mei-05">Mei-05</option>
+                    <option value="Juni-06">Juni-06</option>
+                    <option value="Juli-07">Juli-07</option>
+                    <option value="Agustus-08">Agustus-08</option>
+                    <option value="September-09">September-09</option>
+                    <option value="Oktober-10">Oktober-10</option>
+                    <option value="Novenmber-11">Novenmber-11</option>
+                    <option value="Desember-12">Desember-12</option>
+                  </select>
                 </div>
                 <div class="form-group">
                   <label for="nominalhpp">Nominal HPP </label>
